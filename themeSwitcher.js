c1b28cb8c342f1179b9d00cb70776df57577aaf3
Fenz <uukooo@126.com>
2018-05-22 20:05:32 +0800

    change css url in themeSwitcher relative

--- themeSwitcher.js
+++ themeSwitcher.js
@@ -2,9 +2,9 @@ const themeCss= document.getElementById( 'theme-css', );
 const themeMeta= document.querySelector( 'meta[name=theme-color]', );
 
 export const themes= [
-	{ "name": "default",    "label": "Default",    "color": "hsla(0,0%,93%,1)",     "url": "/themes/default.css", },
-	{ "name": "light_blue", "label": "Light Blue", "color": "hsla(205,100%,80%,1)", "url": "/themes/light-blue.css", },
-	{ "name": "deep_blue",  "label": "Deep Blue",  "color": "hsla(205,80%,60%,1)",  "url": "/themes/deep-blue.css", },
+	{ "name": "default",    "label": "Default",    "color": "hsla(0,0%,93%,1)",     "url": "./themes/default.css", },
+	{ "name": "light_blue", "label": "Light Blue", "color": "hsla(205,100%,80%,1)", "url": "./themes/light-blue.css", },
+	{ "name": "deep_blue",  "label": "Deep Blue",  "color": "hsla(205,80%,60%,1)",  "url": "./themes/deep-blue.css", },
 ];
 
 export function switchTheme( theme, )
