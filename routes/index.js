af48116cada3bb5c6af85c61d3550d8ce7b4f652
Fenz <uukooo@126.com>
2018-05-22 19:02:16 +0800

    make routing relative

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 import Router from '../OvO/routing/Router.js';
+import { currentDir, resolve, } from '../OvO/support/path.js';
 
 const router= new Router();
 
-router.route( 'home', '/', '/pages/index', );
-router.route( 'introduce', '/introduce', '/pages/introduce', );
-router.route( 'model', '/model', '/pages/model', );
-router.route( 'model-demo', '/model-demo', '/pages/model-demo', );
+const dir= currentDir();
+
+router.pageDir= resolve( currentDir(), '../pages', );
+router.basePath= resolve( currentDir(), '..', );
+
+router.route( 'home', '/', 'index', );
+router.route( 'introduce', '/introduce', );
+router.route( 'model', '/model', );
+router.route( 'model-demo', '/model-demo', );
 
 export default router;
