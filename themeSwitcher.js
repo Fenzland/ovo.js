0886ee20b24ff7b68238d85dabadea9e879fdc15
Fenz <uukooo@126.com>
2018-05-23 23:47:35 +0800

    add several themes

--- themeSwitcher.js
+++ themeSwitcher.js
@@ -5,9 +5,12 @@ const themeMeta= document.querySelector( 'meta[name=theme-color]', );
 const storage= new Storage( localStorage, 'theme', );
 
 export const themes= [
-	{ "name": "default",    "label": "Default",    "color": "hsla(0,0%,93%,1)",     "url": "./themes/default.css", },
+	{ "name": "default",    "label": "Default",    "color": "hsla(0,0%,93%,1)",     "url": "./themes/default.css",    },
 	{ "name": "light_blue", "label": "Light Blue", "color": "hsla(205,100%,80%,1)", "url": "./themes/light-blue.css", },
-	{ "name": "deep_blue",  "label": "Deep Blue",  "color": "hsla(205,80%,60%,1)",  "url": "./themes/deep-blue.css", },
+	{ "name": "deep_blue",  "label": "Deep Blue",  "color": "hsla(205,80%,60%,1)",  "url": "./themes/deep-blue.css",  },
+	{ "name": "warm",       "label": "Warm",       "color": "hsla(45,62%,66%,1)",   "url": "./themes/warm.css",       },
+	{ "name": "green",      "label": "Green",      "color": "hsla(120,62%,66%,1)",  "url": "./themes/green.css",      },
+	{ "name": "berry",      "label": "Berry",      "color": "hsla(330,100%,72%,1)", "url": "./themes/berry.css",      },
 ];
 
 export function switchTheme( theme, )
