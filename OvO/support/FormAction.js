492ba156cccc99fdd5c0f54fb456a8a599e5ab95
Fenz <uukooo@126.com>
2018-05-28 21:21:42 +0800

    add Request class, FormAction extends it

--- OvO/support/FormAction.js
+++ OvO/support/FormAction.js
@@ -1,35 +1,10 @@
-import { Indicator, ReIndicator, } from './Indicator.js';
+import Request from './Request.js';
 
-const URL= Symbol( 'url', );
-const METHOD= Symbol( 'method', );
-const LISTENERS= Symbol( 'listeners', );
-const DEFAULT_LISTENER= Symbol( 'default_listener', );
-const EMIT= Symbol( 'emit', );
-const STATUS= Symbol( 'status', );
-
-export default class Link extends Function
+export default class FormAction extends Request
 {
-	constructor( url, method='POST', )
+	request( form, )
 	{
-		super();
-		
-		this[URL]= url;
-		this[METHOD]= method;
-		this[LISTENERS]= [];
-		
-		return new Proxy( this, {
-			apply( target, context, [ e, ], )
-			{
-				target.push( context );
-				
-				return true;
-			}
-		}, );
-	}
-	
-	push( form, )
-	{
-		const method= (form.getAttribute( 'method', )||'').toUpperCase() || this[METHOD];
+		const method= (form.getAttribute( 'method', )||'').toUpperCase() || this.method;
 		
 		let body;
 		switch( form.encoding )
@@ -43,8 +18,8 @@ export default class Link extends Function
 			break;
 		}
 		
-		fetch(
-			this[URL],
+		return fetch(
+			this.url,
 			{
 				method,
 				headers: {
@@ -52,42 +27,6 @@ export default class Link extends Function
 				},
 				body: body,
 			},
-		).then( resp=> this[EMIT]( resp, ), );
-	}
-	
-	when( status, callback, )
-	{
-		const listener= new ReIndicator();
-		
-		listener[STATUS]= status;
-		
-		this[LISTENERS].push( listener, );
-		
-		return listener.then( callback, );
-	}
-	
-	else( callback, )
-	{
-		const listener= new ReIndicator();
-		
-		this[DEFAULT_LISTENER]= listener;
-		
-		return listener.then( callback, );
-	}
-	
-	[EMIT]( resp, )
-	{
-		for( let listener of this[LISTENERS] )
-		{
-			if( listener[STATUS]===resp.status )
-				return listener.resolve( resp, );
-		}
-		if( this[DEFAULT_LISTENER] )
-			this[DEFAULT_LISTENER].resolve( resp, );
-	}
-	
-	toString()
-	{
-		return 'javascript:;';
+		);
 	}
 }
---
+++ OvO/support/Request.js
@@ -0,0 +1,87 @@
+import { ReIndicator, } from './Indicator.js';
+
+const URL= Symbol( 'url', );
+const METHOD= Symbol( 'method', );
+const LISTENERS= Symbol( 'listeners', );
+const DEFAULT_LISTENER= Symbol( 'default_listener', );
+const EMIT= Symbol( 'emit', );
+const STATUS= Symbol( 'status', );
+
+export default class Request extends Function
+{
+	constructor( url, method='POST', )
+	{
+		super();
+		
+		this[URL]= url;
+		this[METHOD]= method;
+		this[LISTENERS]= [];
+		
+		return new Proxy( this, {
+			apply( target, context, [ e, ], )
+			{
+				target.request( context ).then( resp=> target[EMIT]( resp, ), );
+				
+				return true;
+			}
+		}, );
+	}
+	
+	request( body, headers={}, )
+	{
+		return fetch(
+			this[URL],
+			{
+				method: this[METHOD],
+				headers,
+				body: body,
+			},
+		);
+	}
+	
+	when( status, callback, )
+	{
+		const listener= new ReIndicator();
+		
+		listener[STATUS]= status;
+		
+		this[LISTENERS].push( listener, );
+		
+		return listener.then( callback, );
+	}
+	
+	else( callback, )
+	{
+		const listener= new ReIndicator();
+		
+		this[DEFAULT_LISTENER]= listener;
+		
+		return listener.then( callback, );
+	}
+	
+	[EMIT]( resp, )
+	{
+		for( let listener of this[LISTENERS] )
+		{
+			if( listener[STATUS]===resp.status )
+				return listener.resolve( resp, );
+		}
+		if( this[DEFAULT_LISTENER] )
+			this[DEFAULT_LISTENER].resolve( resp, );
+	}
+	
+	get url()
+	{
+		return this[URL];
+	}
+	
+	get method()
+	{
+		return this[METHOD];
+	}
+	
+	toString()
+	{
+		return 'javascript:;';
+	}
+}
