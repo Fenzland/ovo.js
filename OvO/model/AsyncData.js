44aa204a721d0bbc1423b0c1b1c77b1fba927649
Fenz <uukooo@126.com>
2018-08-28 21:32:53 +0800

    extend method "then","catch","finally" of Promise, accept more params "temp" "rejected" and returns a AsyncData object

--- OvO/model/AsyncData.js
+++ OvO/model/AsyncData.js
@@ -36,3 +36,38 @@ export default class AsyncData extends Promise
 		this[REJECTED]= rejected
 	}
 }
+
+{
+	const thenOrig= Promise.prototype.then;
+	
+	Promise.prototype.then= function( onResolve, onReject=undefined, temp=undefined, rejected=undefined, ) {
+		
+		if(!( onReject instanceof Function ))
+			temp= onReject, rejected= temp;
+		
+		if( temp===undefined && rejected===undefined )
+			return thenOrig.call( this, onResolve, onReject, );
+		else
+			return new AsyncData( thenOrig.call( this, onResolve, onReject, ), temp, rejected, );
+	}
+	
+	const catchOrig= Promise.prototype.catch;
+	
+	Promise.prototype.catch= function( onReject, temp=undefined, rejected=undefined, ) {
+		
+		if( temp===undefined && rejected===undefined )
+			return catchOrig.call( this, onReject, );
+		else
+			return new AsyncData( catchOrig.call( this, onReject, ), temp, rejected, );
+	}
+	
+	const finallyOrig= Promise.prototype.finally || function( atLast ){ return thenOrig.call( this, atLast, atLast, ); };
+	
+	Promise.prototype.finally= function( atLast, temp=undefined, rejected=undefined, ) {
+		
+		if( temp===undefined && rejected===undefined )
+			return finallyOrig.call( this, atLast, );
+		else
+			return new AsyncData( finallyOrig.call( this, atLast, ), temp, rejected, );
+	}
+}
