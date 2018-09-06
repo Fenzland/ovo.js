78a6cbad22f84c28922763f0c4ff2f9822377e66
Fenz <uukooo@126.com>
2018-09-06 17:14:03 +0800

    add planet route

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.route( 'home', '/', 'index', );
 router.route( 'introduce', '/introduce', );
 router.route( 'model', '/model', );
 router.route( 'model-demo', '/model-demo', );
+router.route( 'planet', '/model-demo/planets/{planet:Int}', );
 
 export default router;
