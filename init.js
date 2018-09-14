ef63916051a66b0d419feddc8cabc30b66b04e26
Fenz <uukoo@163.com>
2018-09-15 00:43:51 +0800

    polyfix Map.prototype.achieve() get a value if has, or make value and set then return

--- init.js
+++ init.js
@@ -3,3 +3,16 @@ window.v= ( a, ...o )=> (console.log( a.valueOf?a.valueOf():a, ...o.map( x=> x.v
 
 window.timeout= ( time, value, )=> new Promise( ( resolve, reject, )=> setTimeout( ()=> resolve( value, ), time, ), );
 window.nextFrame= value=> new Promise( ( resolve, reject, )=> requestAnimationFrame( ()=> resolve( value, ), ), );
+
+Map.prototype.achieve= function( key, maker, ){
+	if( this.has( key, ) )
+		return this.get( key, );
+	else
+	{
+		const value= maker();
+		
+		this.set( key, value, );
+		
+		return value;
+	}
+};
